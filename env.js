86f1f07148a6b150a596d69960f602caeda53482
J5mmy <j5mmy@hotmail.com>
2024-06-26 20:13:52 +0800

    Update env.js

--- env.js
+++ env.js
@@ -37,7 +37,7 @@ const BUNDLE_ID = 'com.obytes'; // ios bundle id
 const PACKAGE = 'com.obytes'; // android package name
 const NAME = 'ObytesApp'; // app name
 const EXPO_ACCOUNT_OWNER = 'obytes'; // expo account owner
-const EAS_PROJECT_ID = 'c3e1075b-6fe7-4686-aa49-35b46a229044'; // eas project id
+const EAS_PROJECT_ID = '88df3b96-005e-4bb3-96fd-a624c9e0bfa1'; // eas project id
 const SCHEME = 'obytesApp'; // app scheme
 
 /**
